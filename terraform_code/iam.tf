73e7fb0b8686499368e5a505898896961392e2f4
KaviyaManikandan <100072155+KaviyaManikandan@users.noreply.github.com>
2024-03-14 14:05:18 +0530

    Update iam.tf

--- terraform_code/iam.tf
+++ terraform_code/iam.tf
@@ -1,4 +1,3 @@
-# main.tf
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
