ed642ac73972448b002fd719323cee2bfbcd18df
KaviyaManikandan <100072155+KaviyaManikandan@users.noreply.github.com>
2024-03-28 09:07:01 +0530

    Update iam.tf

--- terraform_code/iam.tf
+++ terraform_code/iam.tf
@@ -3,18 +3,12 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
+data "aws_iam_role" "existing_ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
 }
 
+
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  role       = "${data.aws_iam_role.existing_ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
